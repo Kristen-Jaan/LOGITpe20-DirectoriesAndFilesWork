865140fa6f8a196437f761da3b83623239cbefbf
Opilane TTHK <opilane@tthk.ee>
2020-11-13 09:59:00 +0200

    first commit

---
+++ Friday/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Friday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Good Morning!");
+            string FridayPath = @"C:\Users\opilane\Friday\sampleDirectories.txt";
+            string SamplesPath = @"C:\Users\opilane\Samples";
+
+            string[] allDirectories = Directory.GetDirectories(SamplesPath);
+
+            string[] NestedDirectories = Directory.GetDirectories(SamplesPath, "*", SearchOption.AllDirectories);
+            ReadArray(NestedDirectories);
+
+            File.WriteAllLines(FridayPath, NestedDirectories);
+
+
+            
+
+        }
+        public static void ReadArray(string[] array)
+        {
+            foreach(string line in array)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
---
+++ FridayFiles/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace FridayFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Good Morning!");
+            string FridayPath = @"C:\Users\opilane\Friday\samplesFiles.txt";
+            string SamplesPath = @"C:\Users\opilane\Samples";
+
+            string[] files = Directory.GetFiles(SamplesPath, "*.*", SearchOption.AllDirectories);
+            ReadArray(files);
+            File.WriteAllLines(FridayPath, files);
+        }
+
+        public static void ReadArray(string[] array)
+        {
+            foreach(string line in array)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
